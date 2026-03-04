7e79014e1db72a9914c1a2ba0732c1c455186991
Camila1plus1 <230103310@sdu.edu.kz>
2026-03-04 21:43:23 +0500

    Initial project conf

---
+++ config.js
@@ -0,0 +1,24 @@
+// Global Configuration Settings
+const APP_ENV = "production";
+const DEBUG_MODE = false;
+
+const CYCNET_API_BASE = "https://api.cycnet.local/v1";
+const CYCNET_TIMEOUT_MS = 5000;
+
+// FortiGate Firewall Integration
+const FORTI_GATEWAY = "192.168.1.254";
+const FORTI_PORT = 443;
+const FORTI_USE_SSL = true;
+
+// External Services & Auth
+// TODO: Never commit this to public repo!
+const CYCNET_API_TOKEN = "cycnet{git_commits_never_forget}"; 
+const MAX_LOGIN_ATTEMPTS = 3;
+
+module.exports = {
+    APP_ENV,
+    DEBUG_MODE,
+    OMNISYNC_API_BASE,
+    FORTI_GATEWAY,
+    CYCNET_API_TOKEN
+};
